0df237127dc73ec10ba469d75d9205a2de997e48
Kyle Hawkins <kylehawk2@gmail.com>
2018-09-21 09:23:23 -0700

    First Commit

---
+++ Controllers/DojoSurveyController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Dojo_Survey.Controllers
+{
+    public class DojoSurveyController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        [Route("result")]
+        public IActionResult result(string name, string location, string langauge, string comment){
+            ViewBag.name = name;
+            ViewBag.location = location;
+            ViewBag.langauge = langauge;
+            ViewBag.comment = comment;
+            Console.WriteLine(ViewBag.name + ViewBag.location + ViewBag.langauge + ViewBag.comment);
+            return View("result");
+        }
+    }
+}
